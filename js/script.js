b9bc3c9b46c2d4a5f929ad6e2893a3bd65a538e7
nahid <nahidraj.web@gmail.com>
2023-06-13 20:08:20 +0600

    blog page responsive

--- js/script.js
+++ js/script.js
@@ -87,6 +87,26 @@ $(function () {
     slidesToScroll: 1,
     prevArrow: '<i class="fas left icon fa-chevron-left"></i>',
     nextArrow: '<i class="fas right icon fa-chevron-right"></i>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   // blog details tour slider js
